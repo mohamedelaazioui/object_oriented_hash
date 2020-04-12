2dfb6ea5435d1c44f6192deaf8e5422458f2c9ef
mohamedelaazioui <mohamed.elaazioui@gmail.com>
2020-04-12 17:12:17 +0200

    Initial commit

---
+++ lib/object_oriented_hash.rb
@@ -0,0 +1,6 @@
+require "object_oriented_hash/version"
+
+module ObjectOrientedHash
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/object_oriented_hash/version.rb
@@ -0,0 +1,3 @@
+module ObjectOrientedHash
+  VERSION = "0.1.0"
+end
